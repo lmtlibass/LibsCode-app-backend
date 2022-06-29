46b8dc429c6af8f394e211d37715d354d530c5cc
libasse <lmtlibasse@gmail.com>
2022-06-29 03:35:41 +0200

    gestions des rôles et permissions

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['auth', 'role:admin'])->group(function(){
+
+});
+
+Route::middleware(['auth', 'role:createur'])->group(function(){
+
+});
+
+Route::middleware(['auth', 'role:apprenant'])->group(function(){
+
 });
